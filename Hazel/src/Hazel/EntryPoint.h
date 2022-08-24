6f0110a472a327cedf690f96b9a0e51ef1559b00
Jordan <jordanelder10@gmail.com>
2022-08-23 22:18:18 -0500

    Added spdlog and Wrapper Macros

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,7 +5,10 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Hazel Engine!");
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Engine Initialized!");
+	int a = 5;
+	HZ_INFO("Hello World! Var={0}", a);
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
