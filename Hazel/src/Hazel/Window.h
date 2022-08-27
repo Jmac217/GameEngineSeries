f4499c1e10d02dc7b01864af7219f7d104485971
Jordan <jordanelder10@gmail.com>
2022-08-27 02:25:27 -0500

    Added LayerStack system

--- Hazel/src/Hazel/Window.h
+++ Hazel/src/Hazel/Window.h
@@ -13,7 +13,7 @@ namespace Hazel {
 		unsigned int Width;
 		unsigned int Height;
 
-		WindowProps(const std::string& title = "Hazel Engine",
+		WindowProps(const std::string& title = "Mac's Hazel Engine",
 			        unsigned int width = 1280,
 			        unsigned int height = 720)
 			: Title(title), Width(width), Height(height)
