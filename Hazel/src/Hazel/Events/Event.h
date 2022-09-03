018c89cf7f7f9c58ac84b8f60fd44dc25d819144
Jordan <jordanelder10@gmail.com>
2022-09-03 16:53:28 -0500

    Added Multiprocessor flag to Premake, fixed bind target in OpenGLIndexBuffer, Input.h is a singleton, removed instances of std::function in Event.h

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -53,16 +53,13 @@ namespace Hazel {
 
 	class EventDispatcher
 	{
-		template<typename T>
-		using EventFn = std::function<bool(T&)>;
 	public:
 		EventDispatcher(Event& event)
 			: m_Event(event)
 		{
 		}
-
-		template<typename T>
-		bool Dispatch(EventFn<T> func)
+		template<typename T, typename F>
+		bool Dispatch(const F& func)
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
