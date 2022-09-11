4cd0410246e1df7122a6c20c005bd3cf06e13742
Jordan <jordanelder10@gmail.com>
2022-09-10 19:04:38 -0500

    refactored camera as an entity

--- Hazel/src/Hazel/Scene/Components.h
+++ Hazel/src/Hazel/Scene/Components.h
@@ -2,7 +2,20 @@
 
 #include <glm/glm.hpp>
 
+#include "Hazel/Renderer/Camera.h"
+
 namespace Hazel {
+
+	struct CameraComponent
+	{
+		Camera Camera;
+		bool Primary = true; // TODO: think about moving to Scene
+
+		CameraComponent() = default;
+		CameraComponent(const CameraComponent&) = default;
+		CameraComponent(const glm::mat4& projection)
+			: Camera(projection) {}
+	};
 
 	struct TagComponent
 	{
