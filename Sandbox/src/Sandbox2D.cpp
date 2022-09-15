47c92cd86949940667421625ddf5c11ed30327c4
Jmac217 <jordanelder10@gmail.com>
2022-09-15 17:53:50 -0500

    Fixed Sandbox

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -5,7 +5,7 @@
 #include <glm/gtc/type_ptr.hpp>
 
 Sandbox2D::Sandbox2D()
-	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f)
+	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f), m_SquareColor({ 0.2f, 0.3f, 0.8f, 1.0f })
 {
 }
 
@@ -14,8 +14,6 @@ void Sandbox2D::OnAttach()
 	HZ_PROFILE_FUNCTION();
 
 	m_CheckerboardTexture = Hazel::Texture2D::Create("assets/textures/Checkerboard.png");
-	m_OASLettering = Hazel::Texture2D::Create("assets/textures/OASLettering.png");
-
 }
 
 void Sandbox2D::OnDetach()
@@ -39,25 +37,28 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts)
 	}
 
 	{
-		HZ_PROFILE_SCOPE("Renderer Draw");
+		static float rotation = 0.0f;
+		rotation += ts * 50.0f;
 
-#define OAS_ENABLED 1
-#if OAS_ENABLED
-		static float rotation = 120.0f;
-		rotation += ts * -5.0f;
+		HZ_PROFILE_SCOPE("Renderer Draw");
 		Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		Hazel::Renderer2D::DrawRotatedQuad({ 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_OASLettering, 1.0f);
-		for (float y = -2.0f; y <= 2.0f; y += 0.5f)
+		Hazel::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Hazel::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Hazel::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, m_SquareColor);
+		Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.0f }, m_CheckerboardTexture, 10.0f);
+		Hazel::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 20.0f);
+		Hazel::Renderer2D::EndScene();
+
+		Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
+		for (float y = -5.0f; y < 5.0f; y += 0.5f)
 		{
-			for (float x = -2.0f; x <= 2.0f; x += 0.5f)
+			for (float x = -5.0f; x < 5.0f; x += 0.5f)
 			{
-				glm::vec4 color = { (x + 1.0f) / 10.0f, (y + 3.0f) / 10.0f, (y + 2.0f) / 10.0f, 0.7f };
-				Hazel::Renderer2D::DrawQuad({ x, y }, { 0.46f, 0.46f }, color);
+				glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.7f };
+				Hazel::Renderer2D::DrawQuad({ x, y }, { 0.45f, 0.45f }, color);
 			}
 		}
 		Hazel::Renderer2D::EndScene();
-		//  End of Sandbox2D::OnUpdate()
-#endif
 	}
 }
 
@@ -78,7 +79,7 @@ void Sandbox2D::OnImGuiRender()
 	ImGui::End();
 }
 
-void Sandbox2D::OnEvent(Hazel::Event & e)
+void Sandbox2D::OnEvent(Hazel::Event& e)
 {
 	m_CameraController.OnEvent(e);
 }
